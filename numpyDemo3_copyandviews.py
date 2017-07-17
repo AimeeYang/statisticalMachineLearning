17b08ccad202b3489904519a691d7d76ac2f29f4
aimeeyang <aimeeyang@pptv.com>
2017-07-17 13:54:02 +0800

    init numpy demo 3 copies and views

---
+++ numpyDemo3_copyandviews.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+###
+#   Copies and Views
+#       When operating and manipulating arrays, their data is sometimes
+#   copied into a new array and sometimes not. There are three cases:
+#       I. No Copy at All
+###
+
+###
+# I. No copy at all
+#       assignment: eg a=b; a,b point to the same object, and if b change a change.
+#
+###
+
+###
+#
+### 

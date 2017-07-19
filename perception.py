e795a0206b23c50473352a881f1fdbffac3be8b5
aimeeyang <aimeeyang@pptv.com>
2017-07-19 18:18:15 +0800

    init perception method

--- perception.py
+++ perception.py
@@ -19,22 +19,64 @@ import pandas as pd
 import tensorflow as tf
 from LoadHelper import *
 
-learningRate = 0.001
-trainPath = "./data/iris.data.txt"
+learningRate = 0.01
+trainPath = "./data/iris.data.csv"
 
 # type: train, dev, test
 # path: data file path
 def loadData(type, path):
     x,y = pythonlibLoad(path)
-    return 0
+    x1 = x[0:99:5,0:2]
+    print(x1)
+    y1 = y[0:99:5]
+
+    y1 = list(map(renamelabel,y1))
+    print(y1)
+    x1 = np.array(x1)
+    y1 = np.array(y1)
+    return x1,y1
+
+def renamelabel(l):
+    return {
+        2:-1,
+        1: 1
+    }.get(l,-1)
 
 # 基于随机梯度下降 + 所选误分类点到超平面的距离
 # 超平面：kw+b
 def lossFunc(x, k, b):
     return 0
 
-def perception(): # TODO parameter that can be used to different np.array.shape
-    data = loadData("train",trainPath)
+'''f(x) = sign(wx+b)'''
+def perception(w, b, learnRate): # TODO parameter that can be used to different np.array.shape
+    x,y = loadData("train",trainPath)
+    hasWrong=True
+    while(hasWrong):
+        # TODO how rand choose wrong point
+        rundomcount=20
+        rundomtry=0
+        while(rundomtry < rundomcount):
+            r = np.random.randint(0,20,1,dtype='int')
+            if (w.T.dot(x[r])+b)*y[r] < 0:
+                w = w + learningRate*y[r]*x[r]
+                b = b + learningRate*y[r]
+                break
+            else:
+                rundomtry+=1
+        if(rundomtry >= rundomcount):
+            for i in range(y.shape[0]):
+                if (w.T.dot(x[r]) + b) * y[r] < 0:
+                    w = w + learningRate * y[r] * x[r]
+                    b = b + learningRate * y[r]
+                    break
+            if i >= y.shape[0]:
+                hasWrong = False
+        else:
+            continue
 
 
+if __name__ == "__main__":
+    w=np.array([0,0]) #超参数初值
+    b=0
+    perception(w,b, learningRate)
 
--- preprocessing.py
+++ preprocessing.py
@@ -9,17 +9,24 @@ import numpy as np
 # So that load data with numpy or pandas will be convenient.
 ###
 
-def preprocess_changeLabelType(orifile, newfile, convertfunc):
-    data = np.loadtxt(orifile,delimiter=',',skiprows=0)
+def preprocess_changeLabelType(orifile, newfile,convertLabelfunc):
+    data = np.genfromtxt(orifile ,delimiter=',', dtype=None,
+                         names=('sepal_length','sepal_width','petal_length','petal_width','class'),skip_header=1) # 注意skiprows start from 1
     print(data.shape)
     print(data.dtype)
     print(data.size)
     x=[]
     y=[]
+    # x = data[:99,0:1]
+    # y = map(convertLabelfunc,data[:99,4])
     for i in range(data.shape[0]):
         print(i)
         print(data[i])
-        break;
+        if(data[i][4] != "Iris-virginica"):
+            x.append(list(data[i])[0:4])
+            y.append(convertLabelfunc(data[i][4]))
+    np.savetxt(newfile,np.column_stack((x,y)),delimiter=',')
+    #TODO 如何使数据类型保持不变
 
 
 ### for iris data
@@ -27,9 +34,9 @@ def strLabelCovertToIntLabel(labelstr):
     ### in python3 use dictionary instead of switch,
     ### below with default value -1 when labelstr is not found
     return{
-        'Iris-setosa': 1,
-        'Iris-versicolor': 2,
-        'Iris-virginica': 3bv
+        b'Iris-setosa': 1,
+        b'Iris-versicolor': 2,
+        b'Iris-virginica': 3
     }.get(labelstr,-1) # -1 is the default value when labelstr is not found
 
     # return {
@@ -41,5 +48,5 @@ def strLabelCovertToIntLabel(labelstr):
 # below for test.
 if __name__ == "__main__":
     orifile = "./data/iris.data.txt"
-    newfile = "./data/iris.cov.txt"
+    newfile = "./data/iris.cov.csv"
     preprocess_changeLabelType(orifile,newfile, strLabelCovertToIntLabel)

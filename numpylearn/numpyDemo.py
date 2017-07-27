b8cac4ed5b49fada111fcf9902c033de0a239bbb
aimeeyang <aimeeyang@pptv.com>
2017-07-27 10:19:16 +0800

    add numpy reshape -1 usage

--- numpylearn/numpyDemo.py
+++ numpylearn/numpyDemo.py
@@ -67,6 +67,25 @@ print(np.sort(a3, axis=1))
 # print("np.sort(a3, axis=0):")
 # print(np.sort(a3, axis=2))
 
+###
+# reshape use of -1 : 只是为了方便，减少运算(因为自动算出其余维度)
+#       eg. np.arange(0, 60, 10).reshape((-1, 1))
+#         <=> np.arange(0, 60, 10).reshape((6, 1)) # 此处自动算出行的个数
+print(np.arange(0, 60, 10).shape)
+print(np.arange(0, 60, 10).reshape(-1))
+print(np.arange(0, 60, 10).reshape(-1).shape)
+print(np.arange(0, 60, 10).reshape((-1, 1)))
+print(np.arange(0, 60, 10).reshape((-1, 1)).shape)
+print(np.arange(0, 60, 10).reshape((6, 1)))
+print(np.arange(0, 60, 10).reshape((-1, 3)))
+print(np.arange(0, 60, 10).reshape((-1, 3)).shape)
+print(np.arange(0, 60, 10).reshape((-1, 1)) + np.arange(6))
+# [[ 0  1  2  3  4  5]
+#  [10 11 12 13 14 15]
+#  [20 21 22 23 24 25]
+#  [30 31 32 33 34 35]
+#  [40 41 42 43 44 45]
+#  [50 51 52 53 54 55]]
 
 
 # print("a.ndim: ", end='')
